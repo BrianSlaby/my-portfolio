803f3b2cdf822746d0af6177ddeb00ea15b151e3
Brian Slaby <brian.slaby@gmail.com>
2024-03-26 19:48:22 -0400

    updated favicon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
                     </a>
                 </div>
             </div>
-            <p className="small-text">Background image by <a target="_blank" href="https://www.svgbackgrounds.com/set/free-svg-backgrounds-and-patterns/"> SVGBackgrounds.com</a>.  Email Vectors image by <a target="_blank" href="https://www.vecteezy.com/free-vector/email">Vecteezy</a>.</p>
+            <p className="small-text">Background image by <a target="_blank" href="https://www.svgbackgrounds.com/set/free-svg-backgrounds-and-patterns/"> SVGBackgrounds.com</a>.  <a target="_blank" href="https://icons8.com/icon/84648/resume-website">Portfolio</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>.  Email Vectors image by <a target="_blank" href="https://www.vecteezy.com/free-vector/email">Vecteezy</a>.</p>
 
             <p className="small-text">Updated 2024</p>
         </footer>
