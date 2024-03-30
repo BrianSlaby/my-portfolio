09fe3272b6b0b588c0e42d6e49c1487258d3dced
Brian Slaby <brian.slaby@gmail.com>
2024-03-30 13:57:26 -0400

    Added new tab target on Project Links component, adjusted align items for Home sections.

--- src/components/project-page/ProjectLinks.tsx
+++ src/components/project-page/ProjectLinks.tsx
@@ -13,13 +13,13 @@ export default function ProjectLinks({ liveUrl, githubUrl }: projectLinksProps):
             <a 
                 className="link-btn-style link-btn-dark" 
                 href={liveUrl}
-            
+                target="_blank"
             >Link to Live Site</a>
 
             <a 
                 className="link-btn-style link-btn-dark" 
                 href={githubUrl}
-            
+                target="_blank"
             >GitHub Repository</a>
         </div>
     )
