64e00b61b0196aabf5def776d9467a6054959c57
Brian Slaby <brian.slaby@gmail.com>
2024-04-05 10:47:02 -0400

    added comments specifying where each exported type is used

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-
+// components/home/Project.tsx
 type projectComponentProps = {
     src: string,
     alt: string,
@@ -7,16 +7,19 @@ type projectComponentProps = {
     description: string
 }
 
+// components/project-page/ProjectHero.tsx
 type projectHeroHeaderProps = {
     title: string,
     description: string
 }
 
+// components/project-page/ProjectLinks.tsx
 type projectLinksProps = {
     liveUrl: string,
     githubUrl: string
 }
 
+// pages/*each individual project file*
 type galleryImage = {
     url: string,
     alt: string,
@@ -25,6 +28,7 @@ type galleryImage = {
     isCurrent: boolean
 }
 
+// components/Gallery.tsx
 type galleryProps = {
     galleryImages: galleryImage[]
 }
