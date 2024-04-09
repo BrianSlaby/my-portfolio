b4539a3e3524146499239afeb46c6b779377725c
Brian Slaby <brian.slaby@gmail.com>
2024-04-09 15:42:23 -0400

    updated Gallery component so that advancing gallery via scrolling vs clicking the buttons both work consistently

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -11,15 +11,11 @@ export default function Gallery({ galleryImages }: galleryProps): React.JSX.Elem
     const [ fwdBtnClass, setFwdBtnClass ] = useState<"hidden" | "">("")
 
     const galleryRef = useRef<HTMLDivElement | null>(null)
-    // const touchStartX = useRef<number | null>(null)
-    const scrollIncrement = galleryRef.clientWidth
-    // figure this out above
-    // ALSO, after the scroll event is handled and currentIndex is assigned
-        // based on figure in view, set scrollPosition = currentIndex * scrollIncrement
-    // obviously implement debouncing after everything else is handled
-    // set state in useEffect vs handleScroll
 
-    console.log(galleryRef)
+    let scrollIncrement: number = 750
+    if (galleryRef.current) {
+        scrollIncrement = galleryRef.current.clientWidth
+    }
 
     function toggleGalleryForward(): void {
         setCurrentIndex((prevIndex) => prevIndex + 1)
@@ -49,89 +45,29 @@ export default function Gallery({ galleryImages }: galleryProps): React.JSX.Elem
         }
     }
 
-    // function handleTouchStart(e: TouchEvent): void {
-    //     touchStartX.current = e.touches[0].clientX
-    // }
-
-    // function handleTouchScroll(e: TouchEvent): void {
-    //     setIsScrolling(true)
-    //     if (touchStartX.current !== null) {
-    //         const touchEndX = e.touches[0].clientX
-    //         const difference = touchStartX.current - touchEndX
-    //         if (difference > 50) {
-    //             toggleGalleryForward()
-    //         } else if (difference < -50) {
-    //             toggleGalleryBackward()
-    //         }
-    //         touchStartX.current = null
-    //     }
-    //     setIsScrolling(false)
-    // }
-
-    // function handleWheelScroll(e: WheelEvent): void {
-    //     setIsScrolling(true)
-    //     console.log("isScrolling", isScrolling)
-    //     if (e.deltaX !== 0) {
-    //         if (e.deltaX > 50) {
-    //             toggleGalleryForward()
-    //         } else if (e.deltaX < -50) {
-    //             toggleGalleryBackward()
-    //         }
-    //     }
-    //     setIsScrolling(false)
-    // }
-
-    function handleScroll(event: React.UIEvent<HTMLElement>) {
-        const target = event.target as HTMLElement
+    function handleScroll(event: React.UIEvent<HTMLElement>): void {
         const parentEl = event.currentTarget as HTMLElement
-        const { scrollLeft, scrollWidth, clientWidth } = target
-        const position = Math.ceil(scrollLeft / (scrollWidth - clientWidth) * 100)
-
         const figures = document.querySelectorAll(".gallery-figure")
         figures.forEach((figure, index) => {
-            // figure out how to get index from dataset later
-            //console.log("figure as HTMLElement", figure as HTMLElement)
                 if (isElementInView(figure as HTMLElement, parentEl)) {
                     setCurrentIndex(index)
+                    setScrollPosition(scrollIncrement * index)
                 }
-            
         })
+    }  
 
-        setScrollPosition(scrollLeft)
-    }
-
-    // function debounce(func: any, delay: number = 300) {
-    //     let timer: number | undefined
-    //     return (...args: any[]) => {
-    //         if (!timer) {
-    //             func.apply(args) 
-    //             console.log("debounce function worked")
-    //         }
-    //         clearTimeout(timer)
-    //         timer = setTimeout(() => {
-    //             timer = undefined
-    //         }, delay)
-    //     }
-    // }   
-
-    function isElementInView(el: HTMLElement, parent: HTMLElement | null) {
+    function isElementInView(el: HTMLElement, parent: HTMLElement | null): boolean {
         if (!parent) {
             return false
         }
 
         const rect = el.getBoundingClientRect()
         const parentRect = parent?.getBoundingClientRect()
-        
-        // const windowHeight = window.innerHeight || document.documentElement.clientHeight
-        // const windowWidth = window.innerWidth || document.documentElement.clientWidth
-
 
         return (
             rect.left === parentRect.left
         )
     }
-
-    
 
     useEffect(() => {
         if (currentIndex === 0) {
@@ -149,58 +85,10 @@ export default function Gallery({ galleryImages }: galleryProps): React.JSX.Elem
     }, [currentIndex])
 
     useEffect(() => {
-        // const figures = document.querySelectorAll(".gallery-figure")
-        // figures.forEach((figure, index) => {
-        //     // figure out how to get index from dataset later
-        //     console.log("figure as HTMLElement", figure as HTMLElement)
-        //         if (isElementInView(figure as HTMLElement, galleryRef)) {
-        //             setCurrentIndex(index)
-        //         }
-            
-        // })
-
-        console.log("currentIndex", currentIndex)
-        console.log("scrollPosition", scrollPosition)
-
-    }, [scrollPosition])
-
-    // useEffect(() => {
-    //     if (galleryRef.current) {
-    //         galleryRef.current.addEventListener("touchstart", handleTouchStart)
-    //         galleryRef.current.addEventListener("touchmove", debounce(() => handleTouchScroll))
-    //         galleryRef.current.addEventListener("wheel", debounce(() => handleWheelScroll))
-    //     }
-    //     return () => {
-    //         if (galleryRef.current) {
-    //             galleryRef.current.removeEventListener("touchstart", handleTouchStart)
-    //             galleryRef.current.removeEventListener("touchmove", debounce(() => handleTouchScroll))
-    //             galleryRef.current.removeEventListener("wheel", debounce(() => handleWheelScroll))
-    //         }
-    //     }
-    // }, [])
-
-    // useEffect(() => {
-    //     const figures = document.querySelectorAll(".gallery-figure")
-    //     figures.forEach((figure, index) => {
-    //         if (isElementInView(figure as HTMLElement)) {
-    //             setCurrentIndex(index)
-    //         }
-    //     })
-
-    //     if (galleryRef.current) {
-    //         galleryRef.current.addEventListener("touchstart", handleTouchStart)
-    //         galleryRef.current.addEventListener("touchmove", debounce(() => handleTouchScroll))
-    //         galleryRef.current.addEventListener("wheel", debounce(() => handleWheelScroll))
-    //     }
-
-    //     return () => {
-    //         if (galleryRef.current) {
-    //             galleryRef.current.removeEventListener("touchstart", handleTouchStart)
-    //             galleryRef.current.removeEventListener("touchmove", debounce(() => handleTouchScroll))
-    //             galleryRef.current.removeEventListener("wheel", debounce(() => handleWheelScroll))
-    //         }
-    //     }
-    // }, [isScrolling])
+        if (galleryRef.current) {
+            scrollIncrement = galleryRef.current.clientWidth
+        }
+    }, [galleryRef.current?.clientWidth])
 
     return (
         <div className="gallery-outer-wrapper">
@@ -224,10 +112,9 @@ export default function Gallery({ galleryImages }: galleryProps): React.JSX.Elem
 
             {
                 galleryImages.map(figure => {
-                    const currentClass = figure.index === currentIndex ? "current" : ""
                     return (
                         <figure 
-                            className={`gallery-figure ${currentClass}`}
+                            className={`gallery-figure`}
                             key={figure.index}
                             data-index={figure.index}
                         >
